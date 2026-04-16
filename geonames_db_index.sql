2cd9149b05e285d1b2fe8abe885e9d74e33a7dfc
Sven Lieber <Sven.Lieber@gmail.com>
2026-04-16 11:09:34 +0200

    Added new composite key and removed duplicate keys #1

--- geonames_db_index.sql
+++ geonames_db_index.sql
@@ -1,28 +1,12 @@
 -- Added by Sven Lieber on 2024-10-15
+-- removed duplicate keys on 2026-04-16 https://github.com/kbrbe/GeoNames-MySQL-DataImport/issues/1
 
--- Create index on name in the geoname table
-CREATE INDEX idx_geoname_name ON geoname (name);
 
--- Create index on country in the geoname table
-CREATE INDEX idx_geoname_country ON geoname (country);
-
--- Create composite index on name and country in the geoname table
+-- 2026-02-23: Create composite index on name and country in the geoname table to improve query performance
+-- See https://github.com/kbrbe/geonames-lookup/issues/7
 CREATE INDEX idx_geoname_name_country ON geoname (name, country);
 
--- Create index on geonameid in the admin1CodesAscii table
-CREATE INDEX idx_admin1_geonameid ON admin1CodesAscii (geonameid);
 
--- Create index on geonameid in the admin2Codes table
-CREATE INDEX idx_admin2_geonameid ON admin2Codes (geonameid);
-
--- Create index on geonameid in the alternatename table
-CREATE INDEX idx_alternatename_geonameid ON alternatename (geonameid);
-
--- Create index on name in the alternatename table
-CREATE INDEX idx_alternatename_name ON alternatename (alternateName);
-
--- Create index on geonameid in the continentCodes table
-CREATE INDEX idx_continent_geonameid ON continentCodes (geonameid);
 
 -- Create index on geonameid in the countryinfo table
 CREATE INDEX idx_country_geonameid ON countryinfo (geonameId);
